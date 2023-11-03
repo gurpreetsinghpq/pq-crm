2129c1b0c62fde4577c941933a02e32d9e2cf5b0
rajpurple <rajgopal.jakhmola@purplequarter.com>
2023-11-03 13:47:51 +0530

    update: base url

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    NEXT_PUBLIC_BASE_URL : "https://api.elixir.purplequarter.co"
+    // NEXT_PUBLIC_BASE_URL : "https://api.elixir.purplequarter.co"
+    NEXT_PUBLIC_BASE_URL : "https://api.elixir.stage.purplequarter.co"
 
 }
 
