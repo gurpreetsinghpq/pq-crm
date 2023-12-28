d23cf596f4a32a7a7aa77895b01365a837e066fd
rajpurple <rajgopal.jakhmola@purplequarter.com>
2023-12-28 12:14:33 +0530

    update: color scheme interchanged godfather and rockstar

--- src/components/icons/labels.js
+++ src/components/icons/labels.js
@@ -1,6 +1,6 @@
-export const ROCKSTAR_CLASS = "text-pink-600 bg-pink-50 border-pink-500"
+export const ROCKSTAR_CLASS = "text-purple-700 bg-purple-50 border-purple-600"
 export const HUSTLER_CLASS = "text-success-700 bg-success-50 border-success-600"
-export const GODFATHER_CLASS = "text-purple-700 bg-purple-50 border-purple-600"
+export const GODFATHER_CLASS = "text-pink-600 bg-pink-50 border-pink-500"
 // export const SEGEMENT_COMMON_CLASS = "text-xs mr-[10px] px-[6px] py-[2px] border border-[1px] rounded-[6px] font-medium"
 export const SEGEMENT_COMMON_CLASS = "text-sm mr-[10px] pl-[8px] pr-[10px] py-[4px] border border-[1px] rounded-[6px] font-medium"
 
